46976dc068f0ba0eb39cf653c8ac6e54916e0827
Lampros Fasoulas <lfasoula@auth.gr>
2024-12-08 19:41:15 +0200

    Day 8 is done

--- 2024/Day8/main.go
+++ 2024/Day8/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-    const length = 50
+    const length =50
     const filename = "input.txt"
     file,_ := os.Open(filename)
     defer file.Close()
@@ -32,29 +32,61 @@ func main() {
         }
         j++
     }
+    var part2list []string
     //fmt.Println(len(list))
-    check(&list,&sum,&array)
-    //fmt.Println(list)
+    check(&list,&sum,&array,&part2list)
+    part2(&part2list,&array)
+    var sul int
+    var sult2 int
     for _,v:=range array{
         fmt.Println(v)
+        for _,k:=range v{
+            if k != "."{
+                sul++
+            }
+            if k == "#"{
+                sult2++
+            }
+        }
     }
-    fmt.Println(sum)
+    //fmt.Println(sum)
+    //fmt.Println(sul)
+    //fmt.Println("Number of all # ",sult2)
 }
-func check(arr *[]string,sum *int,horse *[][]string){
+func part2(arr *[]string,horse *[][]string){
+    for _,v:=range *arr{
+        x,_ := strconv.Atoi(strings.Split(v,":")[0])
+        y,_ := strconv.Atoi(strings.Split(v,":")[1])
+        diffx,_ := strconv.Atoi(strings.Split(v,":")[2])
+        diffy,_ := strconv.Atoi(strings.Split(v,":")[3])
+        //fmt.Println("X: ",x," Y: ",y," DiffX: ",diffx," DiffY: ",diffy)
+        for true{
+            if x>=0 && x<len(*horse) && y>=0 && y<len(*horse){
+                (*horse)[x][y] = "#"
+                x += diffx
+                y += diffy
+            }else{
+                break
+            }
+        }
+    }
+}
+func check(arr *[]string,sum *int,horse *[][]string,part2 *[]string){
     var found []string
     for _,v:=range *arr{
-        fmt.Println("Checking ",v)
+        //fmt.Println("Checking ",v)
         sym := strings.Split(string(v), ":")[0]
         x,_ := strconv.Atoi(strings.Split(v, ":")[1])
         y,_ := strconv.Atoi(strings.Split(v, ":")[2])
-        fmt.Println("Value of X: ",x," Value of Y: ",y)
+        //fmt.Println("Value of X: ",x," Value of Y: ",y)
         for i,k:=range *horse{
             for j,v:=range k{
                 diffv := x-i
                 diffh := y-j
                 if diffv == 0 && diffh == 0{continue}
                 if sym == string(v)&& x+diffv>=0 && x+diffv<len(*horse) && y+diffh>=0 && y+diffh<len(*horse) {
-                    fmt.Println("Found antinode #"," at ",x+diffv,y+diffh," with ",diffv," ",diffh)
+                    //fmt.Println("Found antinode "," at ",x+diffv,y+diffh," with ",diffv," ",diffh)
+                    *part2 = append(*part2,fmt.Sprintf("%d:%d:%d:%d",x+diffv,y+diffh,diffv,diffh))
                     if !isThere(&found,x+diffv,y+diffh){
                         found = append(found,fmt.Sprintf("%d:%d",x+diffv,y+diffh))
                         *sum ++
@@ -63,9 +95,9 @@ func check(arr *[]string,sum *int,horse *[][]string){
             }
         }
     }
-    fmt.Println(*arr)
-    fmt.Println("List found ",found)
-    fmt.Println("Length of found: ",len(found))
+    //fmt.Println(*arr)
+    //fmt.Println("List found ",found)
+    //fmt.Println("Length of found: ",len(found))
     //fmt.Println("The final result is: ",*sum-errors)
 }
 func isThere (list *[]string, x int, y int) bool{
