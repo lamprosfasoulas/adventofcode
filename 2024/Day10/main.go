3df3ba62055d9a20af045786e60cbafa4c040ed3
Lampros Fasoulas <lfasoula@auth.gr>
2024-12-13 19:25:23 +0200

    Day 10 is done, now all that is left is Day Day12

--- 2024/Day10/main.go
+++ 2024/Day10/main.go
@@ -1,6 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+var moves = [][2]int {
+    {0,-1},
+    {0,1},
+    {-1,0},
+    {1,0},
+}
+
+var nines int
 func main(){
+    var board = make([][]int,53)
+    for i:=range board{
+        board[i] = make([]int,53)
+    }
+    file,_:=os.Open("day10")
+    defer file.Close()
+    var sum int
+    sc := bufio.NewScanner(file)
+    i:=0
+    for sc.Scan(){
+        for j,v := range sc.Bytes(){
+            board[i][j] = int(v-byte('0'))
+        }
+        fmt.Println(board[i])
+        i++
+    }
+    for i:=range board{
+        for j:=range board[i]{
+            if board[i][j] == 0{
+                nines = 0
+                score(board,i,j)
+                sum += nines
+                fmt.Println(sum)
+            }
+        }
+    }
+}
+func score(board [][]int,i,j int,/*found map[[2]int]bool*/){
+    found := make(map[[2]int]bool)
+    if board[i][j] == 9 && found[[2]int{i,j}] == false{
+        nines++
+        found[[2]int{i,j}] = true
+    }
+    fmt.Println(i,j,"Value:",board[i][j])
+    for _,m := range moves{
+        x,y := i+m[0],j+m[1]
+        if x>=0 && x<len(board) && y>=0 && y<len(board){
+            if board[x][y] == board[i][j]+1{
+                score(board,x,y)
+            }else{
+                continue
+            }
+        }
+    }
+    fmt.Println("Nines:",nines)
 }
--- 2024/Day12/main.go
+++ 2024/Day12/main.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main(){
-    file,_:=os.Open("input")
+    var letter string
+    file,_:=os.Open("input2")
     defer file.Close()
-    fmt.Println(string(file))
+    var chart []string
+    //var chart = make([][]string,20)
+    /*
+    for i:=range chart{
+        chart[i] = make([]string,20)
+    }
+*/
+    sc := bufio.NewScanner(file)
+    i:=0
+    for sc.Scan(){
+        line := ""
+        for _,v := range sc.Text(){
+            fmt.Println(string(v))
+            if string(v) == letter{
+                 line += string(v)
+            }else{
+                if letter == ""{
+                    letter = string(v)
+                    line += "|"
+                    line += string(v)
+                    fmt.Println("haha we are here")
+                }else{
+                    letter = string(v)
+                    line += "|"
+                    line += string(v)
+                }
+            }
+
+        }
+        line += "|"
+        chart = append(chart,line)
+        line = strings.Repeat("-",len(line))
+        chart = append(chart,line)
+        line = ""
+        i++
+    }
+    for _,v := range chart{
+        fmt.Println(v)
+    }
 }
